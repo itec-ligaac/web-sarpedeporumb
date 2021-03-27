1ec5c875c321db7def8166c6110ccecf55187c53
womywomwoo <womywomwoo@gmail.com>
2021-03-28 00:40:06 +0200

    call axios on form submit

--- sarpedeporumb-frontend/src/components/registerForm.vue
+++ sarpedeporumb-frontend/src/components/registerForm.vue
@@ -1,26 +1,40 @@
 <template>
-  <form class="log-in-form">
-    <h4 class="text-center">Register with your email</h4>
+  <form class="log-in-form" v-on:submit.prevent="onRegister">
+    <h3 class="text-center">Register with your email</h3>
     <label>Name
-      <input type="text" placeholder="somebody">
+      <input name="username" type="text" placeholder="username">
     </label>
     <label>Email
-      <input type="email" placeholder="somebody@example.com">
+      <input name="email" type="email" placeholder="me@sarpe.xyz">
     </label>
     <label>Password
-      <input type="password" placeholder="Password">
+      <input name="password" type="password" placeholder="Password">
     </label>
     <input id="show-password" type="checkbox"><label for="show-password">Show password</label>
-    <p><input type="submit" class="button expanded" value="Log in"></p>
+    <p><input type="submit"
+     class="button expanded" value="Log in"></p>
     <p class="text-center"><a href="#">Forgot your password?</a></p>
   </form>
 </template>
 
 <script>
-// @ is an alias to /src
+import axios from 'axios';
 
 export default {
   name: 'Login',
+  methods: {
+    onRegister: (el) => {
+      const username = el.target.username.value;
+      const email = el.target.email.value;
+      const password = el.target.password.value;
+
+      axios.post('sarpe.xyz/api/v1/register', {
+        username,
+        email,
+        password,
+      });
+    },
+  },
 };
 </script>
 
