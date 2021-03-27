21408e67fe46cc77299de692b33876351891528f
Rizea Vlad <vlad.rizea.stefan@gmail.com>
2021-03-27 20:37:04 +0200

    add new nav

--- sarpedeporumb-frontend/src/App.vue
+++ sarpedeporumb-frontend/src/App.vue
@@ -1,13 +1,37 @@
 <template>
   <div id="app">
 
-    <div class="top-nav-bar-wrapper">
-      <div class="top-nav-bar">
-        <p class="top-nav-bar-title">SARPE TRAVEL</p>
-        <div class="top-nav-bar-links">
-        </div>
-      </div>
-    </div>
+<nav class="top-bar" data-topbar role="navigation">
+  <ul class="title-area">
+    <li class="name">
+      <h1><a href="#">My Site</a></h1>
+    </li>
+     <!-- Remove the class "menu" to just have icon alone -->
+    <li class="toggle-topbar menu-icon"><a href="#">
+      <span>Menu</span></a>
+    </li>
+  </ul>
+
+  <section class="top-bar-section">
+    <!-- Right Nav Section -->
+    <ul class="right">
+      <li class="active"><a href="#">Right Button Active</a></li>
+      <li class="has-dropdown">
+        <a href="#">Right Button Dropdown</a>
+        <ul class="dropdown">
+          <li><a href="#">First link in dropdown</a></li>
+          <li class="active"><a href="#">Active link in dropdown</a></li>
+        </ul>
+      </li>
+    </ul>
+
+    <!-- Left Nav Section -->
+    <ul class="left">
+      <li><a href="#">Left Nav Button</a></li>
+    </ul>
+  </section>
+</nav>
+
     <router-view/>
     <footer id="footer">
       <div>
