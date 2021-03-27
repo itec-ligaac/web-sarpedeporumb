6b3394b6b273959db3fdddbcbf0aa84363314d96
Rizea Vlad <vlad.rizea.stefan@gmail.com>
2021-03-27 23:13:09 +0200

    add register form component

---
+++ sarpedeporumb-frontend/src/components/registerForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <form class="log-in-form">
+    <h4 class="text-center">Register with your email</h4>
+    <label>Name
+      <input type="text" placeholder="somebody">
+    </label>
+    <label>Email
+      <input type="email" placeholder="somebody@example.com">
+    </label>
+    <label>Password
+      <input type="password" placeholder="Password">
+    </label>
+    <input id="show-password" type="checkbox"><label for="show-password">Show password</label>
+    <p><input type="submit" class="button expanded" value="Log in"></p>
+    <p class="text-center"><a href="#">Forgot your password?</a></p>
+  </form>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Login',
+};
+</script>
+
+<style lang='scss'>
+</style>
