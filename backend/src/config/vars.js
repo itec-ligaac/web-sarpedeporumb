cc35fc18ec7ab2a5ac52bc85e44e3d4f2ea3d279
womywomwoo <womywomwoo@gmail.com>
2021-03-27 16:07:17 +0200

    do not allow empty values

--- backend/src/config/vars.js
+++ backend/src/config/vars.js
@@ -1,9 +1,7 @@
 const path = require('path');
 
 // import .env variables
-require('dotenv-safe').config({
-  allowEmptyValues: true,
-}).load({
+require('dotenv-safe').load({
   path: path.join(__dirname, '../../.env'),
   sample: path.join(__dirname, '../../.env.example'),
 });
