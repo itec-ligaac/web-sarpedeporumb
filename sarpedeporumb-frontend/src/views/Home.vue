3b9b723f98fbe09c9c4c87235d63725ed05257a4
Rizea Vlad <vlad.rizea.stefan@gmail.com>
2021-03-27 19:35:52 +0200

    create home page

--- sarpedeporumb-frontend/src/views/Home.vue
+++ sarpedeporumb-frontend/src/views/Home.vue
@@ -1,18 +1,98 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="home-page">
+  <div class="home-page-content">
+    <div class="start-button-text">
+      <h1><b>Plan your dream holiday with the virus in mind today!</b></h1>
+      <router-link class="button-start" to="/register"><b>Sign Up</b></router-link>
+    </div>
+
+    <div class="home-page-spacer"></div>
+    <img class="jumbo-image" src="../assets/images/travel_woman.svg" alt="Travel picture of woman">
   </div>
+</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld,
-  },
+
 };
 </script>
+
+<style lang='scss'>
+
+@import "../app";
+@import "../assets/flex.scss";
+@import "../assets/screen-size.scss";
+@import url('https://fonts.googleapis.com/css2?family=Karla&display=swap');
+.home-page{
+  width: 100%;
+  height: auto;
+  min-height: 100vh;
+  background: $white;
+  top: 0;
+  .home-page-content{
+    width: 100%;
+    height: 100vh;
+    max-width: 1620px;
+    margin: 0 auto;
+    align-content: center;
+    @include lg{
+      @include flexbox();
+      @include justify-content(center);
+      @include flex-direction(column);
+    }
+    @include flexbox();
+    @include justify-content(center);
+    @include flex-direction(row);
+
+    .start-button-text{
+      @include flexbox();
+      @include justify-content(flex-start);
+      @include flex-direction(column);
+      height: auto;
+      margin: auto 0;
+      h1{
+        text-align: center;
+        font-family: 'Karla', sans-serif;
+        font-size: 50px;
+        width: 350px;
+        margin: 20px auto;
+      }
+      .button-start{
+        border: 0;
+        border-radius: 5px;
+        padding: 12px 40px;
+        margin: 20px auto;
+        box-shadow: 0px 3px 15px rgba(0, 0, 0, 0.2);
+        font-family: inherit;
+        font-size: 1em;
+        background-color: #6c63ff;
+        color: $white;
+        cursor: pointer;
+        transition: all 0.4s ease 0s;
+        &:hover{
+          -webkit-box-shadow: 0px 5px 40px -10px rgba(0, 0, 0, 0.57);
+          -moz-box-shadow: 0px 5px 40px -10px rgba(0, 0, 0, 0.57);
+          box-shadow: 5px 40px -10px rgba(0, 0, 0, 0.57);
+          transition: all 0.4s ease 0s;
+        }
+      }
+
+    }
+
+    .home-page-spacer{
+      width: 10%;
+    }
+
+    .jumbo-image{
+      height: auto;
+      width: 400px;
+      margin: 0 auto;
+    }
+  }
+}
+
+</style>
