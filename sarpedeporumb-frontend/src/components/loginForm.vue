4e712d3dbb892f8a6bea3d5a855808447501d58c
Rizea Vlad <vlad.rizea.stefan@gmail.com>
2021-03-27 20:35:51 +0200

    add login view

---
+++ sarpedeporumb-frontend/src/components/loginForm.vue
@@ -0,0 +1,25 @@
+<template>
+  <form class="log-in-form">
+    <h4 class="text-center">Log in with you email account</h4>
+    <label>Email
+      <input type="email" placeholder="somebody@example.com">
+    </label>
+    <label>Password
+      <input type="password" placeholder="Password">
+    </label>
+    <input id="show-password" type="checkbox"><label for="show-password">Show password</label>
+    <p><input type="submit" class="button expanded" value="Log in"></p>
+    <p class="text-center"><a href="#">Forgot your password?</a></p>
+  </form>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Login',
+};
+</script>
+
+<style lang='scss'>
+</style>
---
+++ sarpedeporumb-frontend/src/views/Login.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="grid-y">
+  <div class="cell large-auto">One</div>
+  <div class="cell large-auto">Two</div>
+  <div class="cell large-auto">Three</div>
+  <div class="cell large-auto">Four</div>
+  <div class="cell large-auto">Five</div>
+  <div class="cell large-auto">Six</div>
+</div>
+</template>
+
+<script>
+// @ is an alias to /src
+import loginForm from '../components/loginForm.vue';
+
+export default {
+  name: 'Home',
+  comments: {
+    loginForm,
+  },
+};
+</script>
+
+<style lang='scss'>
+</style>
